8c83f14cbda240219e43888d750a316430233857
coro-kunnnnnn <finkazhr@gmail.com>
2024-12-21 00:59:56 +0700

    log auth

--- app/src/main/java/com/submission/stoup/ui/login/LoginActivity.kt
+++ app/src/main/java/com/submission/stoup/ui/login/LoginActivity.kt
@@ -1,21 +1,69 @@
 package com.submission.stoup.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.submission.stoup.R
 import com.submission.stoup.databinding.ActivityLoginBinding
+import com.submission.stoup.ui.main.MainActivity
+import com.submission.stoup.ui.viewmodelfactory.ViewModelFactory
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val loginViewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        loginViewModel.loginResult.observe(this){ result ->
+            if (result != null) {
+                val token = result.loginResult?.token
+                if (!token.isNullOrEmpty()) {
+                    Toast.makeText(this, "Login successful! Token: $token", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Token is missing", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        loginViewModel.isLoading.observe(this) { isLoading ->
+            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        }
+
+        loginViewModel.errorMessage.observe(this) { errorMessage ->
+            if (!errorMessage.isNullOrEmpty()) {
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setupNavigation() {
+        binding.btnLogin.setOnClickListener{
+            val email = binding.edEmail.text.toString().trim()
+            val pw = binding.edPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && pw.isNotEmpty()){
+                loginViewModel.login(email, pw)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/submission/stoup/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/submission/stoup/ui/login/LoginViewModel.kt
@@ -1,7 +1,54 @@
 package com.submission.stoup.ui.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.submission.stoup.data.remote.pref.UserModel
 import com.submission.stoup.data.remote.repository.UserRepository
+import com.submission.stoup.data.remote.response.LoginResponse
+import kotlinx.coroutines.launch
 
-class LoginViewModel(private val userRepository: UserRepository): ViewModel() {
+class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    private val _loginResult = MutableLiveData<LoginResponse?>()
+    val loginResult: LiveData<LoginResponse?> = _loginResult
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun login(email: String, pw: String) {
+        _isLoading.value = true
+        viewModelScope.launch {
+            try {
+                val result = userRepository.login(email, pw)
+                if (result.isSuccess) {
+                    val loginResponse = result.getOrNull()
+                    if (loginResponse != null && loginResponse.loginResult?.token != null) {
+                        val user = UserModel(email = email, token = loginResponse.loginResult.token, login = true
+                        )
+                        saveSession(user)
+                        _loginResult.postValue(loginResponse)
+                    } else {
+                        _errorMessage.postValue("Token not found")
+                    }
+                } else {
+                    _errorMessage.postValue(result.exceptionOrNull()?.message ?: "Login failed")
+                }
+            } catch (e: Exception) {
+                _errorMessage.postValue(e.message ?: "Error")
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+
+    private fun saveSession(user: UserModel) {
+        viewModelScope.launch {
+            userRepository.saveSessions(user)
+        }
+    }
 }
