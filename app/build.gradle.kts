270212fbc8262436c8d439ead19fa0bd46362a3b
coro-kunnnnnn <finkazhr@gmail.com>
2025-01-03 23:53:29 +0700

    set loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.paging.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
